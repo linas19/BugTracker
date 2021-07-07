3c7f606ddcfa66adb2f7f34957de573de48bc68c
linas19 <linas.jonika1@gmail.com>
2021-07-07 21:12:43 +0300

    added material ui

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,6 +1,35 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles({
+    signupContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        boxShadow: '5px 5px 15px 5px #000000',
+        paddingBottom: '10px',
+        background: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 2%, rgba(0,208,252,1) 16%, rgba(0,212,255,1) 49%)'
+    },
+    loginContainer: {
+        display: 'flex',
+        flexDirection: 'column',
+        boxShadow: '5px 5px 15px 5px #000000',
+        paddingBottom: '10px',
+        background: 'linear-gradient(90deg, rgba(19,36,0,1) 0%, rgba(90,121,9,1) 2%, rgba(73,252,0,1) 23%)'
+    },
+    button: {
+        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+        border: 0,
+        borderRadius: 3,
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        color: 'white',
+        height: 48,
+        padding: '30px 30px',
+        marginTop: '20px'
+    },
+});
 
 const axios = require('axios');
 const userState = {
@@ -67,59 +96,71 @@ export default function Login() {
                 console.log('Sign up failed')
             })
     }
+    const classes = useStyles();
+
     return (
-        <div>
-            <h1>Signup</h1>
-            <div>Create user</div>
-            <div>
-                <input
-                    type='text'
+
+        <Container>
+            <Container maxWidth="sm" className={classes.signupContainer}>
+                <h1>Signup</h1>
+                <div>
+                    <TextField id="standard-basic"
+                        label="User name"
+                        variant="outlined"
+                        type='text'
+                        name='userName'
+                        placeholder='User Name'
+                        value={state.username}
+                        onChange={e => setState({ ...state, username: e.target.value })}
+                    />
+                    <TextField id="standard-basic"
+                        label="Email"
+                        type='text'
+                        variant="outlined"
+                        name='email'
+                        placeholder='Email'
+                        value={state.email}
+                        onChange={e => setState({ ...state, email: e.target.value })}
+                    />
+                    <TextField id="standard-basic" label="Password" type='text' variant="outlined"
+                        type='text'
+                        name='password'
+                        placeholder='password'
+                        value={state.password}
+                        onChange={e => setState({ ...state, password: e.target.value })}
+                    />
+                    <TextField id="standard-basic"
+                        label="Select a role"
+                        type='text'
+                        variant="outlined"
+                        name='roles'
+                        placeholder='roles'
+                        value={state.roles}
+                        onChange={e => setState({ ...state, roles: [e.target.value] })}
+                    />
+                    <Button variant="contained" color="primary" onClick={submit} className={classes.button}>
+                        Create new user
+                    </Button>
+                </div>
+            </Container>
+            <Container maxWidth="sm" className={classes.loginContainer}>
+                <h1>If you already have an account, please login</h1>
+                <TextField id="standard-basic" label="User Name" type='text' variant="outlined"
                     name='userName'
                     placeholder='User Name'
-                    value={state.username}
-                    onChange={e => setState({ ...state, username: e.target.value })}>
-                </input>
-                <input
-                    type='text'
-                    name='email'
-                    placeholder='Email'
-                    value={state.email}
-                    onChange={e => setState({ ...state, email: e.target.value })}>
-                </input>
-                <input
-                    type='text'
+                    value={loginState.username}
+                    onChange={e => setLoginState({ ...loginState, username: e.target.value })} />
+                <TextField id="standard-basic" label="Password" type='text' variant="outlined"
                     name='password'
                     placeholder='password'
-                    value={state.password}
-                    onChange={e => setState({ ...state, password: e.target.value })}>
-                </input>
-                <input
-                    type='text'
-                    name='roles'
-                    placeholder='roles'
-                    value={state.roles}
-                    onChange={e => setState({ ...state, roles: [e.target.value] })}>
-                </input>
-                <button onClick={submit}>Create new username</button>
-            </div>
-            <h1>Login</h1>
-            <input
-                type='text'
-                name='userName'
-                placeholder='User Name'
-                value={loginState.username}
-                onChange={e => setLoginState({ ...loginState, username: e.target.value })}>
-            </input>
-            <input
-                type='text'
-                name='password'
-                placeholder='password'
-                value={loginState.password}
-                onChange={e => setLoginState({ ...loginState, password: e.target.value })}>
-            </input>
-            <button onClick={submitLogin}>Login</button>
-            { isLoggedIn && <div>Loggedin</div>}
-        </div>
+                    value={loginState.password}
+                    onChange={e => setLoginState({ ...loginState, password: e.target.value })} />
+                <Button variant="contained" color="primary" onClick={submitLogin} className={classes.button}>
+                    Login
+                </Button>
+                {isLoggedIn && <div>Loggedin</div>}
+            </Container>
+        </Container>
     )
 }
 
