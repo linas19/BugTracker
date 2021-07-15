cf09c875084e36841ed71b0fdecb6f1d232ca6ec
linas19 <linas.jonika1@gmail.com>
2021-07-15 13:03:18 +0300

    added material ui

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -3,33 +3,35 @@ import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import Container from '@material-ui/core/Container';
 import { makeStyles } from '@material-ui/core/styles';
+import Avatar from '@material-ui/core/Avatar';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import AssignmentTurnedInTwoToneIcon from '@material-ui/icons/AssignmentTurnedInTwoTone';
+import Typography from '@material-ui/core/Typography';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 
-const useStyles = makeStyles({
-    signupContainer: {
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
         display: 'flex',
         flexDirection: 'column',
-        boxShadow: '5px 5px 15px 5px #000000',
-        paddingBottom: '10px',
-        background: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 2%, rgba(0,208,252,1) 16%, rgba(0,212,255,1) 49%)'
+        alignItems: 'center',
     },
-    loginContainer: {
-        display: 'flex',
-        flexDirection: 'column',
-        boxShadow: '5px 5px 15px 5px #000000',
-        paddingBottom: '10px',
-        background: 'linear-gradient(90deg, rgba(19,36,0,1) 0%, rgba(90,121,9,1) 2%, rgba(73,252,0,1) 23%)'
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.primary.main,
     },
-    button: {
-        background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
-        border: 0,
-        borderRadius: 3,
-        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
-        color: 'white',
-        height: 48,
-        padding: '30px 30px',
-        marginTop: '20px'
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(3),
     },
-});
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
 
 const axios = require('axios');
 const userState = {
@@ -100,66 +102,170 @@ export default function Login() {
 
     return (
 
-        <Container>
+        <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <AssignmentTurnedInTwoToneIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign up
+                </Typography>
+                <form className={classes.form} noValidate>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                required
+                                fullWidth
+                                autoFocus
+                                id="standard-basic"
+                                label="User name"
+                                variant="outlined"
+                                type='text'
+                                name='userName'
+                                placeholder='User Name'
+                                value={state.username}
+                                onChange={e => setState({ ...state, username: e.target.value })}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                required
+                                fullWidth
+                                id="standard-basic"
+                                label="Select a role"
+                                type='text'
+                                variant="outlined"
+                                name='roles'
+                                placeholder='Roles'
+                                value={state.roles}
+                                onChange={e => setState({ ...state, roles: [e.target.value] })}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                label="Email Address"
+                                autoComplete="email"
+                                id="standard-basic"
+                                type='text'
+                                variant="outlined"
+                                name='email'
+                                placeholder='Email'
+                                value={state.email}
+                                onChange={e => setState({ ...state, email: e.target.value })}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                type="password"
+                                autoComplete="current-password"
+                                id="standard-basic"
+                                label="Password"
+                                variant="outlined"
+                                name='password'
+                                placeholder='Password'
+                                value={state.password}
+                                onChange={e => setState({ ...state, password: e.target.value })}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <FormControlLabel
+                                control={<Checkbox value="allowExtraEmails" color="primary" />}
+                                label="I want to receive updates on my tickets and projects via email."
+                            />
+                        </Grid>
+                    </Grid>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                        onClick={submit}
+                    >
+                        Sign Up
+                    </Button>
+                    <Grid container justifyContent="flex-end">
+                        <Grid item>
+                            <Link href="#" variant="body2">
+                                Already have an account? Sign in
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </form>
+            </div>
             <Container maxWidth="sm" className={classes.signupContainer}>
-                <h1>Signup</h1>
-                <div>
-                    <TextField id="standard-basic"
-                        label="User name"
+            </Container>
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Sign in
+                </Typography>
+                <form className={classes.form} noValidate>
+                    <TextField
                         variant="outlined"
-                        type='text'
+                        margin="normal"
+                        required
+                        fullWidth
+                        autoFocus
+                        id="standard-basic" 
+                        label="User Name" 
+                        type='text' 
+                        variant="outlined"
                         name='userName'
                         placeholder='User Name'
-                        value={state.username}
-                        onChange={e => setState({ ...state, username: e.target.value })}
+                        value={loginState.username}
+                        onChange={e => setLoginState({ ...loginState, username: e.target.value })}
                     />
-                    <TextField id="standard-basic"
-                        label="Email"
-                        type='text'
+                    <TextField
                         variant="outlined"
-                        name='email'
-                        placeholder='Email'
-                        value={state.email}
-                        onChange={e => setState({ ...state, email: e.target.value })}
-                    />
-                    <TextField id="standard-basic" label="Password" type='text' variant="outlined"
-                        type='text'
+                        margin="normal"
+                        required
+                        fullWidth
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
                         name='password'
-                        placeholder='password'
-                        value={state.password}
-                        onChange={e => setState({ ...state, password: e.target.value })}
+                        placeholder='Password'
+                        value={loginState.password}
+                        onChange={e => setLoginState({ ...loginState, password: e.target.value })}
                     />
-                    <TextField id="standard-basic"
-                        label="Select a role"
-                        type='text'
-                        variant="outlined"
-                        name='roles'
-                        placeholder='roles'
-                        value={state.roles}
-                        onChange={e => setState({ ...state, roles: [e.target.value] })}
+                    <FormControlLabel
+                        control={<Checkbox value="remember" color="primary" />}
+                        label="Remember me"
                     />
-                    <Button variant="contained" color="primary" onClick={submit} className={classes.button}>
-                        Create new user
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                        onClick={submitLogin}
+                    >
+                        Sign In
                     </Button>
-                </div>
-            </Container>
-            <Container maxWidth="sm" className={classes.loginContainer}>
-                <h1>If you already have an account, please login</h1>
-                <TextField id="standard-basic" label="User Name" type='text' variant="outlined"
-                    name='userName'
-                    placeholder='User Name'
-                    value={loginState.username}
-                    onChange={e => setLoginState({ ...loginState, username: e.target.value })} />
-                <TextField id="standard-basic" label="Password" type='text' variant="outlined"
-                    name='password'
-                    placeholder='password'
-                    value={loginState.password}
-                    onChange={e => setLoginState({ ...loginState, password: e.target.value })} />
-                <Button variant="contained" color="primary" onClick={submitLogin} className={classes.button}>
-                    Login
-                </Button>
-                {isLoggedIn && <div>Loggedin</div>}
-            </Container>
+                    <Grid container>
+                        <Grid item xs>
+                            <Link href="#" variant="body2">
+                                Forgot password?
+                            </Link>
+                        </Grid>
+                        <Grid item>
+                            <Link href="#" variant="body2">
+                                {"Don't have an account? Sign Up"}
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </form>
+            </div>
+            {isLoggedIn && <div>Loggedin</div>}
         </Container>
     )
 }
